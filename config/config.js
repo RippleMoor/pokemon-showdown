0b581fea377879c2078dbe6af97f7c58507a20a4
RippleMoor <3693057522@qq.com>
2026-07-28 19:47:37 +0800

    Add initial Pokémon Showdown configuration file

---
+++ config/config.js
@@ -0,0 +1,17 @@
+/**
+ * Pokémon Showdown config file
+ */
+exports.port = 8000;
+exports.serverid = 'pokemon-showdown';
+
+// 允许所有人访问（你的私服）
+exports.restricted = false;
+
+// 日志级别
+exports.loglevel = 2;
+
+// 自动创建账号（不需要验证邮箱）
+exports.requireemail = false;
+
+// 启动时自动打开浏览器（服务器端无意义，保持 false）
+exports.openbrowser = false;
